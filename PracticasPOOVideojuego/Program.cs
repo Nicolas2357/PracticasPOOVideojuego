0b7ebad4dc688990747d36983d249375775f6695
Nicolas Ponce Carmona <poncecarmonanicolas09@gmail.com>
2026-06-16 15:42:42 -0600

    Agregar archivos de proyecto.

---
+++ PracticasPOOVideojuego/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PracticasPOOVideojuego
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Videojuego juego = new Videojuego();
+
+            juego.Nombre = "The Legend of Zelda";
+            juego.Genero = "Aventura";
+            juego.Precio = 1299;
+            juego.Plataforma = "Nintendo Switch";
+
+            juego.MostrarInfo();
+
+            Console.WriteLine("\nIntentando asignar un precio negativo...");
+
+            juego.Precio = -500;
+
+            juego.MostrarInfo();
+        }
+    }
+}
---
+++ PracticasPOOVideojuego/Videojuego.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PracticasPOOVideojuego
+{
+    internal class Videojuego
+    {
+        private string nombre;
+        private string genero;
+        private double precio;
+        private string plataforma;
+
+        public string Nombre
+        {
+            get { return nombre; }
+            set { nombre = value; }
+        }
+        public string Genero
+        {
+            get { return genero; }
+            set { genero = value; }
+        }
+        public double Precio
+        {
+            get { return precio; }
+            set
+            {
+                if (value >= 0)
+                    precio = value;
+            }
+        }
+        public string Plataforma
+        {
+            get { return plataforma; }
+            set { plataforma = value; }
+        }
+        public void MostrarInfo()
+        {
+            Console.WriteLine("Nombre: " + nombre);
+            Console.WriteLine("Género: " + genero);
+            Console.WriteLine("Precio: $" + precio);
+            Console.WriteLine("Plataforma: " + plataforma);
+        }
+    }
+}
